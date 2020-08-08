e5a5c071999e4e5f5ffc66e897120a698d2d11e8
rodrigo.b.silva <rodrigo.barbosa@fcamara.com.br>
2020-08-08 01:23:21 -0300

    estilização teacher list

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import PageHeader from '../../components/PageHeader';
+
+function TeacherForm(){
+  return (
+    <div id="oage-teacher-form">
+      <PageHeader title="Que incrível que você quer dar aulas" />
+    </div>
+  )
+}
+
+export default TeacherForm;
---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
+
+function TeacherList(){
+  return (
+    <div id="oage-teacher-list">
+      <PageHeader title="Estes são os Proffys disponíveis.">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="week_day">Matéria</label>
+            <input type="text" id="subject" />
+          </div>
+          <div className="input-block">
+            <label htmlFor="week_day">Dia da semana</label>
+            <input type="text" id="week_day" />
+          </div>
+          <div className="input-block">
+            <label htmlFor="time">Hora</label>
+            <input type="text" id="time" />
+          </div>
+
+        </form>
+      </PageHeader>
+    </div>
+  )
+}
+
+export default TeacherList;
