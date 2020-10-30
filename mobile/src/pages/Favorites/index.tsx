d70097ce4bd1335d4534e2c7af83081cb926beee
rodrigo.b.silva <rodrigo.barbosa@fcamara.com.br>
2020-10-30 00:09:49 -0300

    Page Header

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import PageHeader from '../../components/PageHeader';
+
 import styles from './styles';
 
 function Favorites() {
   return (
     <View style={styles.container}>
-      <Text>Favorites</Text>
+      <PageHeader title="Meus Proffys favoritos" />
     </View>
   )
 }
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import PageHeader from '../../components/PageHeader';
+
 import styles from './styles';
 
 function TeacherList() {
   return (
     <View style={styles.container}>
-      <Text>TeacherList</Text>
+      <PageHeader title="Proffys disponíveis" />
     </View>
   )
 }
